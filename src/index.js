c1a40c324ca313b0b761284f9aa41b5f98191046
drbarq <J.Tustin@gmail.com>
2019-11-11 17:43:12 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import ProductsProvider from './context/products-context'
+import './index.css';
+import App from './App';
+
+
+ReactDOM.render(
+  <ProductsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductsProvider>,
+  document.getElementById('root')
+);
+
+
+// redux 
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { combineReducers, createStore } from 'redux';
+// import { BrowserRouter } from 'react-router-dom';
+
+// import './index.css';
+// import App from './App';
+// import productReducer from './store/reducers/products';
+
+// const rootReducer = combineReducers({
+//   shop: productReducer
+// });
+
+// const store = createStore(rootReducer);
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+//   document.getElementById('root')
+// );
